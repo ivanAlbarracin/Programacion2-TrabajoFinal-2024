45b20b2278730929da8879d66c222924c6b21e06
Ivan Albarracin <ivan.albarracin@itscordoba.edu.ar>
2024-11-08 00:31:42 -0300

    Agregamos atributos a la clase city

--- gps_simulation/routes/models.py
+++ gps_simulation/routes/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class City(models.Model):
     name = models.CharField(max_length=100)
+    info_y_bodegas = models.TextField(null=True, blank=True)
+    imagen = models.ImageField(upload_to='static/cities_images/', null=True)
 
     def __str__(self):
         return self.name
